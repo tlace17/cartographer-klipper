a2cd8e867cb9b4fcae87858f09d845604353806d
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2024-01-15 09:31:22 -0700

    Update k1_installer.sh

--- k1_installer.sh
+++ k1_installer.sh
@@ -5,12 +5,8 @@ sh /usr/cartographer-klipper/install.sh
 sed -i '/\[gcode_macro START_PRINT\]/,/\[/gcode_macro START_PRINT\]/d' /usr/data/printer_config/gcode_macro.cfg
 curl -o /usr/data/printer_config/start_end.cfg https://github.com/K1-Klipper/cartographer-klipper/raw/master/start_end.cfg
 sed -i '/\[include printer_params.cfg\]/a\[include cartographer_macro.cfg]\' /usr/data/printer_config/printer.cfg
-if grep -q "include start_macro_KAMP.cfg" your_file.txt; then
-    command_if_exists
+if grep -q "include start_macro_KAMP.cfg" your_file.txt || grep -q "include start_macro.cfg" /usr/data/printer_config/printer.cfg; then
+    sed -i 's/\[include start_(macro|macro_KAMP)\.cfg\]/[include start_stop.cfg]/g' /usr/data/printer_config/printer.cfg
 else
-if grep -q "include start_macro.cfg" your_file.txt; then
-    command_if_exists
-else
-    command_if_not_exists
-fi
+    sed -i '/\[include printer_params.cfg\]/a\[start_end.cfg]\' /usr/data/printer_config/printer.cfg
 fi
