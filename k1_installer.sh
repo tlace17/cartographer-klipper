5818e5ca76ad8fe13d125101931d729e7756ac94
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2024-01-17 15:11:35 -0700

    Update k1_installer.sh

--- k1_installer.sh
+++ k1_installer.sh
@@ -2,16 +2,16 @@
 git clone https://github.com/K1-Klipper/cartographer-klipper.git /usr/data/
 chmod +x /usr/cartographer-klipper/install.sh
 sh /usr/cartographer-klipper/install.sh
-sed -i '/\[gcode_macro START_PRINT\]/,/\[/gcode_macro START_PRINT\]/d' /usr/data/printer_config/gcode_macro.cfg
-curl -o /usr/data/printer_config/start_end.cfg https://github.com/K1-Klipper/cartographer-klipper/raw/master/start_end.cfg
+sed -i '/\[gcode_macro START_PRINT\]/,/\[/gcode_macro START_PRINT\]/d' /usr/data/printer_data/config/gcode_macro.cfg
+curl -o /usr/data/printer_data/config/start_end.cfg https://github.com/K1-Klipper/cartographer-klipper/raw/master/start_end.cfg
 sed -i '/\[include printer_params.cfg\]/a\[include cartographer_macro.cfg]\' /usr/data/printer_config/printer.cfg
 if grep -q "include start_macro_KAMP.cfg" /usr/data/printer_config/printer.cfg || grep -q "include start_macro.cfg" /usr/data/printer_config/printer.cfg; then
     sed -i 's/\[include start_(macro|macro_KAMP)\.cfg\]/[include start_stop.cfg]/g' /usr/data/printer_config/printer.cfg
 else
-    sed -i '/\[include printer_params.cfg\]/a\[start_end.cfg]\' /usr/data/printer_config/printer.cfg
+    sed -i '/\[include printer_params.cfg\]/a\[start_end.cfg]\' /usr/data/printer_data/config/printer.cfg
 fi
 mkdir /usr/data/backups/
-mv /usr/data/printer_config/sensorless.cfg /usr/data/backups/
-curl -o /usr/data/printer_config/sensorless.cfg https://github.com/K1-Klipper/cartographer-klipper/raw/master/sensorless.cfg 
-curl -o /usr/data/printer_config/carto_macro.cfg https://raw.githubusercontent.com/K1-Klipper/cartographer-klipper/master/cartographer_macro.cfg
-sed '/\[mcu\]/i\[include cartographer_macro.cfg]' /usr/data/printer_config/printer.cfg
+mv /usr/data/printer_data/config/sensorless.cfg /usr/data/backups/
+curl -o /usr/data/printer_data/config/sensorless.cfg https://github.com/K1-Klipper/cartographer-klipper/raw/master/sensorless.cfg 
+curl -o /usr/data/printer_data/config/carto_macro.cfg https://raw.githubusercontent.com/K1-Klipper/cartographer-klipper/master/cartographer_macro.cfg
+sed '/\[mcu\]/i\[include cartographer_macro.cfg]' /usr/data/printer_data/config/printer.cfg
