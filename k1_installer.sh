e6f6083b108a69e6b0ca20ab3768501cca4285c3
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2024-02-04 17:52:47 -0700

    Update k1_installer.sh

--- k1_installer.sh
+++ k1_installer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 git config --global http.sslVerify false
-git clone https://github.com/K1-Klipper/cartographer-klipper.git /usr/data/
-chmod +x /usr/cartographer-klipper/install.sh
-sh /usr/cartographer-klipper/install.sh
+git clone https://github.com/K1-Klipper/cartographer-klipper.git /usr/data/cartographer-klipper
+chmod +x /usr/data/cartographer-klipper/install.sh
+sh /usr/data/cartographer-klipper/install.sh
 sed -i '/\[gcode_macro START_PRINT\]/,/\[/gcode_macro START_PRINT\]/d' /usr/data/printer_data/config/gcode_macro.cfg
 wget --no-check-certificate -P  /usr/data/printer_data/config/ https://raw.githubusercontent.com/K1-Klipper/cartographer-klipper/master/start_end.cfg
-sed -i '/\[include printer_params.cfg\]/a\[include cartographer_macro.cfg]\' /usr/data/printer_config/printer.cfg
-if grep -q "include start_macro_KAMP.cfg" /usr/data/printer_config/printer.cfg || grep -q "include start_macro.cfg" /usr/data/printer_config/printer.cfg; then
-    sed -i 's/\[include start_(macro|macro_KAMP)\.cfg\]/[include start_stop.cfg]/g' /usr/data/printer_config/printer.cfg
+sed -i '/\[include printer_params.cfg\]/a\[include cartographer_macro.cfg]\' /usr/data/printer_data/config/printer.cfg
+if grep -q "include start_macro_KAMP.cfg" /usr/data/printer_data/config/printer.cfg || grep -q "include start_macro.cfg" /usr/data/printer_data/config/printer.cfg; then
+    sed -i 's/\[include start_(macro|macro_KAMP)\.cfg\]/[include start_stop.cfg]/g' /usr/data/printer_data/config/printer.cfg
 else
     sed -i '/\[include printer_params.cfg\]/a\[start_end.cfg]\' /usr/data/printer_data/config/printer.cfg
 fi
